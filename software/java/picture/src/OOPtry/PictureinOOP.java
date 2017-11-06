c088948264469a2fae4e499a10557f62d2a2a6e5
Muste17 <33293969+Muste17@users.noreply.github.com>
2017-11-06 14:33:39 +0100

    punktum

--- software/java/picture/src/OOPtry/PictureinOOP.java
+++ software/java/picture/src/OOPtry/PictureinOOP.java
@@ -3,7 +3,7 @@ package OOPtry;
 public class PictureinOOP {
 
     public static void main(String[] args) {
-        Methods p = new Methods(); // object og tilgang til metoder
+        Methods p = new Methods(); // object og tilgang til metoder.
         
         
         
